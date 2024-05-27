4aeec3375d95e4838653ce4be9c1fcc1c1a2c7f9
Rohan Sharma <rohansh2017@gmail.com>
2024-05-27 12:02:27 -0400

    Initial Commit

---
+++ MoodMapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoodMapper
+//
+//  Created by Rohan Sharma on 5/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoodMapper/MoodMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMapperApp.swift
+//  MoodMapper
+//
+//  Created by Rohan Sharma on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
