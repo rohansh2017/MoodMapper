c3f1dc07f1ea349cf5be60706ad03b66fc8a2d24
Rohan Sharma <rohansh2017@gmail.com>
2024-05-30 09:34:22 -0400

    Added Mood Analytics
    Allows users to view a count of their different moods using a button.

---
+++ MoodMapper/AnalyticsView.swift
@@ -0,0 +1,18 @@
+//
+//  AnalyticsView.swift
+//  MoodMapper
+//
+//  Created by Rohan Sharma on 5/28/24.
+//
+
+import SwiftUI
+
+struct AnalyticsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnalyticsView()
+}
