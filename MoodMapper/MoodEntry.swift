e2e91839f724595ed6073e581281ec272f857ad8
Rohan Sharma <rohansh2017@gmail.com>
2024-05-30 16:34:16 -0400

    Paning and Spacing
    Added two separate panes for adding moods and viewing analytics. Moved content a little lower.

---
+++ MoodMapper/MoodEntry.swift
@@ -0,0 +1,8 @@
+//
+//  MoodEntry.swift
+//  MoodMapper
+//
+//  Created by Rohan Sharma on 5/30/24.
+//
+
+import Foundation
---
+++ MoodMapper/MoodLoggingView.swift
@@ -0,0 +1,18 @@
+//
+//  MoodLoggingView.swift
+//  MoodMapper
+//
+//  Created by Rohan Sharma on 5/30/24.
+//
+
+import SwiftUI
+
+struct MoodLoggingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoodLoggingView()
+}
